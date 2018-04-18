8e7fc338bddfafa89ed988dbc314b12255261de0
Nawawish <hydrokidz@gmail.com>
2018-04-18 22:40:16 +0700

    Update .gitignore. Run composer update.

--- autoload.php
+++ autoload.php
@@ -1,11 +1,11 @@
 <?php
 
-// Vendor
-require_once 'Common.php';
-require_once 'Atom.php';
-require_once 'Molecule.php';
-require_once 'Theme.php';
-require_once 'Factory.php';
+// // Vendor
+// require_once 'Common.php';
+// require_once 'Atom.php';
+// require_once 'Molecule.php';
+// require_once 'Theme.php';
+// require_once 'Factory.php';
 
-// App
-require_once 'app/autoload.php';
+// // App
+// require_once 'app/autoload.php';
