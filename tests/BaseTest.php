1105832f26cec4fe8717b27d8110f4ddc0385b1a
Nawawish <hydrokidz@gmail.com>
2018-04-23 14:52:00 +0700

    Create BaseTest.php

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -3,6 +3,9 @@
 use UIFactory\Components\Base;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Test only methods in Base class, not including methods of UIFactory\Component
+ */
 class BaseTest extends TestCase
 {
 	public function testCanBeInstantiated()
@@ -10,15 +13,42 @@ class BaseTest extends TestCase
 		$this->assertInstanceOf(Base::class, new Base());
 	}
 
-	public function testCanAddHtmlMarkupStringByAnonymousFunction()
+	public function testCanAddHtmlMarkupStringUsingAnonymousFunction()
 	{
 		$expected = "<button>Click me!</button>";
 
 		$base = new Base();
-		$base->addMarkup(function($props) use ($expected) {
+		$base->addMarkup(function ($props) use ($expected) {
 			return $expected;
 		});
 
 		$this->assertEquals($expected, $base->get());
+	}
+
+	public function testCanAddHtmlMarkupStringUsingStringCallback()
+	{
+		$expected = "<button>Click me!</button>";
+
+		function button() {
+			return "<button>Click me!</button>";
+		}
+
+		$base = new Base();
+		$base->addMarkup('button');
+
+		$this->assertEquals($expected, $base->get());
+	}
+
+	public function testCanAddProps()
+	{
+		$label = "Don't click me!";
+		$base = new Base();
+		$base->addMarkup(function ($props) {
+			return "<button>$props->label</button>";
+		})->addProps([
+			'label' => $label
+		]);
+
+		$this->assertEquals("<button>$label</button>", $base->get());
 	}
 }
